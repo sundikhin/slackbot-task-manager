9dabd1318816b9472d16cfc7e27a3f685ac41b44
Sundi Myint <sundi.myint@cava.com>
2020-07-08 13:14:18 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Koa = require('koa');
+const app = new Koa();
+const { WebClient } = require('@slack/web-api');
+const ngrok = require('ngrok');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+// Read a token from the environment variables
+const token = process.env.SLACK_TOKEN;
+const slack_url = "https://hooks.slack.com/services/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";
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+(async function() {
+  const url = await ngrok.connect();
+  console.log(url, "URL")
+})();
+
+// Initialize
+const web = new WebClient(token);
+
+let response = [];
+const data = () => {
+  return fetch(slack_url)
+  .then(res => res.json())
+  .then(data => response = data)
+  
+  
+}
+
+data();
+app.listen(3000);
